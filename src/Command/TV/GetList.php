b1ae8ba6e0fbaa2f475738e4de23ff473121fc46
Julian Weaver <julian@finetuned.co.uk>
2025-11-04 16:20:06 +0000

    Add category filter to tv|chunk|snippet|tempate:list commands

--- src/Command/TV/GetList.php
+++ src/Command/TV/GetList.php
@@ -3,6 +3,7 @@
 namespace MODX\CLI\Command\TV;
 
 use MODX\CLI\Command\ListProcessor;
+use Symfony\Component\Console\Input\InputOption;
 
 /**
  * A command to get a list of template variables in MODX
@@ -16,6 +17,28 @@ class GetList extends ListProcessor
 
     protected $name = 'tv:list';
     protected $description = 'Get a list of template variables in MODX';
+
+    protected function getOptions()
+    {
+        return array_merge(parent::getOptions(), array(
+            array(
+                'category',
+                null,
+                InputOption::VALUE_REQUIRED,
+                'Filter by category ID'
+            ),
+        ));
+    }
+
+    protected function beforeRun(array &$properties = array(), array &$options = array())
+    {
+        // Add the category filter
+        if ($this->option('category') !== null) {
+            $properties['category'] = $this->option('category');
+        }
+
+        return parent::beforeRun($properties, $options);
+    }
 
     protected function parseValue($value, $column)
     {
--- src/Command/Template/GetList.php
+++ src/Command/Template/GetList.php
@@ -3,6 +3,7 @@
 namespace MODX\CLI\Command\Template;
 
 use MODX\CLI\Command\ListProcessor;
+use Symfony\Component\Console\Input\InputOption;
 
 /**
  * A command to get a list of templates in MODX
@@ -16,6 +17,28 @@ class GetList extends ListProcessor
 
     protected $name = 'template:list';
     protected $description = 'Get a list of templates in MODX';
+
+    protected function getOptions()
+    {
+        return array_merge(parent::getOptions(), array(
+            array(
+                'category',
+                null,
+                InputOption::VALUE_REQUIRED,
+                'Filter by category ID'
+            ),
+        ));
+    }
+
+    protected function beforeRun(array &$properties = array(), array &$options = array())
+    {
+        // Add the category filter
+        if ($this->option('category') !== null) {
+            $properties['category'] = $this->option('category');
+        }
+
+        return parent::beforeRun($properties, $options);
+    }
 
     protected function parseValue($value, $column)
     {
