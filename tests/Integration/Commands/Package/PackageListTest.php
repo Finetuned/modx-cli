975fc72ffc6e49ccd8c4f455351c51629e355cff
Julian Weaver <julian@finetuned.co.uk>
2026-01-10 14:35:26 +0000

    Added list option coverage for package:list pagination

--- tests/Integration/Commands/Package/PackageListTest.php
+++ tests/Integration/Commands/Package/PackageListTest.php
@@ -31,4 +31,17 @@ class PackageListTest extends BaseIntegrationTest
         $this->assertIsArray($data['results']);
         $this->assertArrayHasKey('total', $data);
     }
+
+    public function testPackageListHonorsPagination()
+    {
+        $data = $this->executeCommandJson([
+            'package:list',
+            '--limit=1',
+            '--start=0'
+        ]);
+
+        $this->assertArrayHasKey('results', $data);
+        $this->assertIsArray($data['results']);
+        $this->assertLessThanOrEqual(1, count($data['results']));
+    }
 }
